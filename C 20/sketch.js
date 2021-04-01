3c1998ae0d1b11fde38b2a5e513204ec2322a34c
JaishalPatel <80263421+JaishalPatel@users.noreply.github.com>
2021-04-01 13:56:49 +0530

    Add files via upload

---
+++ C 20/sketch.js
@@ -0,0 +1,29 @@
+var cat,mouse;
+
+function preload() {
+    
+}
+
+function setup(){
+    createCanvas(1000,800);
+    
+    cat = createSprite(100,100,20,20);
+    mouse = createSprite(300,100,20,20);
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
